c38f4fa4ce1c6f451f87b7c938099c5ac9beeaf9
Gabriel <gabrielleong917@gmail.com>
2024-07-01 10:27:51 -0700

    Added database connection

---
+++ SITracker/Data/TrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SITracker.Data
+{
+    public class TrackerDbContext : DbContext
+    {
+        public TrackerDbContext(DbContextOptions<ApplicationDbContext> context) : base(options)
+        {
+        }
+
+        public DbSet<Adversary> Adversaries { get; set; }
+        public DbSet<GameSession> GameSessions { get; set; }
+        public DbSet<Spirit> Spirits { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- SITracker/Program.cs
+++ SITracker/Program.cs
@@ -1,6 +1,12 @@
+using SITracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Database Connection
+builder.Services.AddDbContext<TrackerDbContext>(options =>
+    options.UseNpsql(builder.Configuration.GetConnectionString("DefaultConnections")));
 
 builder.Services.AddControllers();
 
